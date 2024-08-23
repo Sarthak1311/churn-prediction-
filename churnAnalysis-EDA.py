08c6b792d7b821bbb787e6d2bc13723a1dfcdff7
sarthaktyagi1704@gmail.com <sarthaktyagi1704@gmail.com>
2024-08-23 08:38:38 +0530

    deleted churnAnalysis-EDA

--- churnAnalysis-EDA.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np  
-import seaborn as sns 
-import matplotlib.ticker as mtick  
-import matplotlib.pyplot as plt
-
-data = pd.read_csv("CustomerChurn.csv")
-#print(data.sample(5))
-print(data.shape)
